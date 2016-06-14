afd6b85904aa91c8c25c7df48f7cbb482ef5c064
BrianBend9 <BrianBend9@users.noreply.github.com>
2016-06-14 15:55:43 -0400

    modified js/jquery on load function.

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function() {
+$(document).on("load ready", function() {
   // Fade-in transitions for About and Feature sections
   var tweenAbout = new TweenMax.from('.card-about', 1, {
     css: {autoAlpha:0},
