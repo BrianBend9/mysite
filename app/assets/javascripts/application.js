0b29a3f7ec2c2ba748c38757cdca184b6992bb0d
BrianBend9 <BrianBend9@users.noreply.github.com>
2016-06-15 07:48:51 -0400

    Add bootstrap and fixed top navbar to test mobile display issue.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require ScrollMagic.min.js
 //= require jquery.ScrollMagic.min.js
 //= require debug.addIndicators.js
--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,19 +1,18 @@
-$(document).ready(function() {
-
-// responsive-menu hambuger button toggle
-  "use strict";
-
-  var toggleButton = document.querySelector(".hamburger");
-  var toggleMenu = document.querySelector(".responsive-menu");
-
-  function toggleMenuHandler(toggleButton, toggleMenu) {
-    toggleButton.addEventListener( "click", function(e) {
-      e.preventDefault();
-      toggleButton.classList.toggle("is-active");
-      toggleMenu.classList.toggle("is-active");
-    });
-  }
-
-  toggleMenuHandler(toggleButton, toggleMenu);
-
-});
+// $(document).ready(function() {
+//
+//   "use strict";
+//
+//   var toggleButton = document.querySelector(".hamburger");
+//   var toggleMenu = document.querySelector(".responsive-menu");
+//
+//   function toggleMenuHandler(toggleButton, toggleMenu) {
+//     toggleButton.addEventListener( "click", function(e) {
+//       e.preventDefault();
+//       toggleButton.classList.toggle("is-active");
+//       toggleMenu.classList.toggle("is-active");
+//     });
+//   }
+//
+//   toggleMenuHandler(toggleButton, toggleMenu);
+//
+// });
